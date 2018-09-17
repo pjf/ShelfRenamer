aceee61c6ac578fd94c8febe0d44aa350bd13e5d
Paul Fenwick <pjf@perltraining.com.au>
2018-09-17 10:24:24 +1000

    First attempt at shelf renamer. Untested.

---
+++ Source/Dialog_Rename.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using Verse;
+using RimWorld;
+using System.Reflection;
+
+// Huge thanks to RimFridge, from which much of this code was lifted.
+
+namespace ShelfRenamer
+{
+	public class Dialog_Rename : Window
+    {
+		private string inputText = "";
+		private Building_Storage building;
+		private readonly int maxLength = 28;
+
+		public override Vector2 InitialSize => new Vector2(280f, 175f);
+
+		public Dialog_Rename(Building_Storage building)
+        {
+			this.forcePause = true;
+			this.doCloseX = true;
+			this.closeOnClickedOutside = true;
+			this.absorbInputAroundWindow = true;
+			this.building = building;
+			this.inputText = building.Label;
+		}
+
+		public override void DoWindowContents(Rect inRect)
+		{
+			Text.Font = GameFont.Small;
+			bool enter = false;
+
+            // If the user hits enter, we should accept their new name
+			if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return)
+			{
+				enter = true;
+				Event.current.Use();
+			}
+           
+            // Textfield to type in, populated with the old name.
+			string text = Widgets.TextField(new Rect(0f, 15f, inRect.width, 35f), this.inputText);
+
+            // Don't accept names that are too long
+			if (text.Length < this.maxLength)
+			{
+				this.inputText = text;
+			}
+
+            // If the user hits enter or OK, then stash the label.
+			if (Widgets.ButtonText(new Rect(15f, inRect.height - 35f - 15f, inRect.width - 15f - 15f, 35f), "OK", true, false, true) || enter)
+			{
+				if (this.inputText.Length > 0)
+				{
+					// Label is read-only, so we need to be a little more tricksy by getting the backing variable and assigning to that.
+					var label = typeof(Building_Storage).GetField("<Label>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic);
+					label.SetValue(building, this.inputText);
+					Find.WindowStack.TryRemove(this, true);
+				}
+			}
+		}
+	}
+}
---
+++ Source/ShelfRenamer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using Harmony;
+using UnityEngine;
+
+namespace ShelfRenamer
+{
+	[HarmonyPatch(typeof(Building_Storage))]
+	[HarmonyPatch("GetGizmos")]
+	public static class ShelfRenamer
+    {
+
+		[HarmonyPostfix]
+		public static void RenameShelves(Building_Storage __instance, ref IEnumerable<Gizmo> __result)
+		{
+			// If it has a user-accessible storage tab, then allow renaming.
+
+			// TODO: Don't add a rename button if one already exists.
+
+			if (__instance.StorageTabVisible)
+			{
+				__result.Add(new Command_Action
+				{
+					icon = ContentFinder<Texture2D>.Get("UI/Icons/Rename", true),
+					defaultDesc = "Rename".Translate(),
+					defaultLabel = "Rename".Translate(),
+					activateSound = SoundDef.Named("Click"),
+					action = delegate { Find.WindowStack.Add(new Dialog_Rename(__instance)); }
+                    // groupKey ??
+				});
+			}
+		}
+    }
+}
