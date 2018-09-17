a9426fc14bf6214ebf6458b1b58359ded8b4a893
Paul Fenwick <pjf@perltraining.com.au>
2018-09-17 18:20:43 +1000

    Compress png for Steam Workshop

--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cd Assemblies
 rm Assembly-CSharp* Mono.* mscorlib.dll System.* TextMeshPro-1.0.55.56.0b11.dll UnityEngine.* 0Harmony.dll HugsLib.dll ShelfRenamer.dll.mdb
+cd ../..
+zip -r ShelfRenamer ShelfRenamer/About ShelfRenamer/Assemblies ShelfRenamer/CREDITS ShelfRenamer/Defs ShelfRenamer/LICENSE ShelfRenamer/README.md ShelfRenamer/Source ShelfRenamer/Textures
